857301e66be52d7e308dabbf38416420ed5f952c
jr <jhanarato@gmail.com>
2026-06-14 17:53:23 +0800

    Combine data from both branches.

--- edges.py
+++ edges.py
@@ -1,5 +1,21 @@
 import polars as pl
 
+
 columns = ['_from', '_to', 'from', 'to', 'number', 'remark', 'resembling', 'type']
-main = pl.read_csv('relationships_main.csv', new_columns=columns)
-refactored = pl.read_csv('relationships_refactored.csv', new_columns=columns)
+
+
+def get_main() -> pl.DataFrame:
+    return pl.read_csv('relationships_main.csv', new_columns=columns)
+
+
+def get_refactored() -> pl.DataFrame:
+    return pl.read_csv('relationships_refactored.csv', new_columns=columns)
+
+
+def combine() -> pl.DataFrame:
+    return pl.concat(
+        [
+            get_main().with_columns(pl.lit('main').alias('branch')),
+            get_refactored().with_columns(pl.lit('refactored').alias('branch'))
+        ]
+    )
